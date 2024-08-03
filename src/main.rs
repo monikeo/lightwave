16cdfef8364fcbfd6f0b34993df8dfba169d658e
monikeo281000 <dark.moni.281000@gmail.com>
2024-08-04 01:17:48 +0700

    implement main.rs with cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+
+use lightwave::*;
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+
+    let command = Command::parse();
+    let device_path = match get_backlight_devices() {
+        Some(path) => path,
+        None => {
+            eprintln!(" [-] ERROR: No backlight device found!");
+            return;
+        }
+    };
+    match get_max_brightness(&device_path) {
+        Some(max_brightness) => {
+            match command {
+                Command::Set{value} => {
+                    if let Err(err) = set_brightness(&device_path, value) {
+                        eprintln!(" [-] ERROR: Failed to set brightness: {}", value);
+                    } else {
+                        println!(" [+] Brightness set to {}", value);    
+                    }
+                },
+                Command::Get => {
+                    match read_brightness(&device_path) {
+                        Some(brightness) => {
+                            println!(" [+] Current Brightness {}", brightness);
+                        },
+                        None => {
+                            eprintln!(" [-] ERROR: Failed to read brightness.");
+                        }
+                    }
+                },
+                Command::GetMax => {
+                    println!(" [+] Max Brightness {}", max_brightness);
+                }
+            }
+        },
+        None => {
+            eprintln!(" [-] ERROR: Failed to read maximum brightness.");
+        }
+    }
 }
